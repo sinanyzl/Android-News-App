3d9c1ac66c7c9ee15eee4a631278bc57caac65fe
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-11-06 18:29:25 +0300

    main

--- app/src/main/java/com/example/android_news_app/NewsListAdapter.kt
+++ app/src/main/java/com/example/android_news_app/NewsListAdapter.kt
@@ -1,6 +1,17 @@
 package com.example.android_news_app
 
-class NewsListAdapter {
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+class NewsListAdapter(private val listener : NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+
+
 
 
 
